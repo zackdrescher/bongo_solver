ed210e27aebf7156df14d3c39c02433cac2da666
Zack Drescher <hello@zackdrescher.com>
2024-11-28 22:20:26 -0500

    refactor base letter slot

--- bongo_solver/letter_slot/base_letter_slot.py
+++ bongo_solver/letter_slot/base_letter_slot.py
@@ -2,43 +2,35 @@
 
 from __future__ import annotations
 
-from typing import Any
-
-from typing_extensions import Self
-
 from bongo_solver.letter_tile import LetterTile  # noqa: TC001
 
 
-class BaseLetterSlot:
-    """An abstract base class for a slot containing a letter in a word."""
+class LetterSlot:
+    """A slot to contain a letter in a word."""
 
-    def __new__(cls, *args: tuple[Any], **kwargs: dict[str, Any]) -> Self:  # noqa: ARG003
-        """Create a new instance of the class."""
-        if cls is BaseLetterSlot:
-            msg = f"{BaseLetterSlot.__name__} cannot be instantiated directly."
-            raise TypeError(msg)
-        return super().__new__(cls)
+    def __init__(self, multiplier: int = 1) -> None:
+        """Initialize the letter slot."""
+        self.__multiplier: int = multiplier
 
     letter_tile: LetterTile | None = None
 
-    def place_letter(self, letter_tile: LetterTile) -> None:
-        """Place a letter tile in the slot."""
-        self.letter_tile = letter_tile
-
-    def remove_letter(self) -> None:
-        """Remove the letter tile from the slot."""
-        self.letter_tile = None
-
+    @property
     def score(self) -> int:
         """Return the score of the letter tile in the slot."""
         if self.letter_tile is None:
             return 0
 
-        return self.letter_tile.score
+        return self.letter_tile.score * self.__multiplier
 
+    @property
     def is_empty(self) -> bool:
         """Return True if the slot is empty."""
         return self.letter_tile is None
+
+    @property
+    def is_multiplier(self) -> bool:
+        """Return True if the slot is a multiplier."""
+        return self.__multiplier > 1
 
     def __repr__(self) -> str:
         """Return a string representation of the letter slot."""
--- tests/test_letter_slot/test_base_letter_slot.py
+++ tests/test_letter_slot/test_base_letter_slot.py
@@ -2,74 +2,32 @@
 
 from unittest.mock import MagicMock
 
-import pytest
-
-from bongo_solver.letter_slot import base_letter_slot
+from bongo_solver.letter_slot.base_letter_slot import LetterSlot
 from bongo_solver.letter_tile import LetterTile
-
-
-def test_abstract_letter_slot__new__raises_error() -> None:
-    """Test that BaseLetterSlot.__new__ raises an error."""
-    with pytest.raises(
-        TypeError,
-        match="BaseLetterSlot cannot be instantiated directly.",
-    ):
-        base_letter_slot.BaseLetterSlot()
-
-
-class ConcreteLetterSlot(base_letter_slot.BaseLetterSlot):
-    """A concrete implementation of the BaseLetterSlot class."""
 
 
 def test_letter_slot__init__no_tile() -> None:
     """Test that a letter slot is empty when created."""
-    slot = ConcreteLetterSlot()
-    assert slot.letter_tile is None
-
-
-def test_letter_slot__place_letter() -> None:
-    """Test that a letter tile can be placed in the slot."""
-    # arrange
-    slot = ConcreteLetterSlot()
-    letter_mock = MagicMock(spec=LetterTile)
-
-    # act
-    slot.place_letter(letter_mock)
-
-    # assert
-    assert slot.letter_tile == letter_mock
-
-
-def test_letter_slot__remove_letter() -> None:
-    """Test that a letter tile can be removed from the slot."""
-    # arrange
-    slot = ConcreteLetterSlot()
-    letter_mock = MagicMock(spec=LetterTile)
-    slot.place_letter(letter_mock)
-
-    # act
-    slot.remove_letter()
-
-    # assert
+    slot = LetterSlot()
     assert slot.letter_tile is None
 
 
 def test_letter_slot__score__no_tile() -> None:
     """Test that the score is zero when the slot is empty."""
-    slot = ConcreteLetterSlot()
-    assert slot.score() == 0
+    slot = LetterSlot()
+    assert slot.score == 0
 
 
 def test_letter_slot__score__with_tile() -> None:
     """Test that the score is the score of the letter tile."""
     # arrange
-    slot = ConcreteLetterSlot()
+    slot = LetterSlot()
     letter_mock = MagicMock(spec=LetterTile)
     letter_mock.score = 5
-    slot.place_letter(letter_mock)
+    slot.letter_tile = letter_mock
 
     # act
-    score = slot.score()
+    score = slot.score
 
     # assert
     assert score == 5
@@ -77,19 +35,31 @@ def test_letter_slot__score__with_tile() -> None:
 
 def test_is_empty__is_empty() -> None:
     """Test that is_empty returns True when the slot is empty."""
-    slot = ConcreteLetterSlot()
-    assert slot.is_empty() is True
+    slot = LetterSlot()
+    assert slot.is_empty is True
 
 
 def test_is_empty_not_empty() -> None:
     """Test that is_empty returns False when the slot is not empty."""
     # arrange
-    slot = ConcreteLetterSlot()
+    slot = LetterSlot()
     letter_mock = MagicMock(spec=LetterTile)
-    slot.place_letter(letter_mock)
+    slot.letter_tile = letter_mock
 
     # act
-    is_empty = slot.is_empty()
+    is_empty = slot.is_empty
 
     # assert
     assert is_empty is False
+
+
+def test_is_multiplier__is_multiplier() -> None:
+    """Test that is_multiplier returns True when the slot is a multiplier."""
+    slot = LetterSlot(multiplier=2)
+    assert slot.is_multiplier is True
+
+
+def test_is_multiplier__not_multiplier() -> None:
+    """Test that is_multiplier returns False when the slot is not a multiplier."""
+    slot = LetterSlot()
+    assert slot.is_multiplier is False
